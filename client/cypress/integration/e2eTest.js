7f1c4e3d3f77a8f708fe05638c04fc79a73c0cff
Yuya Satake <yuyasatake@YuyanoMacBook-Pro.local>
2021-01-09 04:23:38 +0900

    cypressのテストを追加

--- client/cypress/integration/e2eTest.js
+++ client/cypress/integration/e2eTest.js
@@ -1,8 +1,9 @@
 describe("e2e", () => {
-  it("game start and  game clear", () => {
+  it("game start and  game clear and go back title button works in Result", () => {
     cy.visit("/");
     cy.wait(1000);
     cy.get("body").trigger("keypress", { key: " " });
+    cy.contains("表示された数字または記号のキーを押してください");
     cy.wait(2000);
     for (let i = 1; i < 11; i++) {
       cy.get("#symbol")
@@ -12,5 +13,16 @@ describe("e2e", () => {
         });
       cy.wait(2000);
     }
+    cy.contains("結果");
+    cy.contains("タイトルに戻る").click();
+    cy.contains("タイピング練習ゲーム");
+  });
+
+  it("go back title button works in Content", () => {
+    cy.visit("/");
+    cy.wait(1000);
+    cy.get("body").trigger("keypress", { key: " " });
+    cy.contains("タイトルに戻る").click();
+    cy.contains("タイピング練習ゲーム");
   });
 });
